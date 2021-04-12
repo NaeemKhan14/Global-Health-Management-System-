b242b956c01e0c99eb56c5e665bcc7bd18d08f83
Naeem Khan <naeemukhan14@gmail.com>
2021-04-12 05:47:25 +0400

    Added default theme style options

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,13 @@ import 'package:ghms/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'Backend/Authentication/authentication_wrapper.dart';
+import 'dart:ui';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
   runApp(MyApp());
 }
 
@@ -16,25 +19,32 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    double screenWidth = window.physicalSize.width;
+
     return MultiProvider(
       providers: [
         Provider<AuthenticationService>(
           create: (_) => AuthenticationService(FirebaseAuth.instance),
         ),
         StreamProvider(
-          create: (context) => context.read<AuthenticationService>().authStateChanges,
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
           initialData: null,
         )
       ],
       child: MaterialApp(
         title: "Global Health Management System",
         theme: ThemeData(
-          primaryColor: kPrimaryColor,
+          primaryColor: colorWhite,
+          accentColor: colorDarkBlue,
+          // Display smaller fonts for smaller screens
+          textTheme: screenWidth < 500 ? textThemeSmall : textThemeDefault,
           scaffoldBackgroundColor: Colors.white,
+          fontFamily: "Montserrat",
         ),
         home: AuthenticationWrapper(),
+
       ),
     );
   }
 }
-
