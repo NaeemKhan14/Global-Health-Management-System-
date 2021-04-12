8c57a7551b570a3785353015b3b1c926d9e75324
Naeem Khan <naeemukhan14@gmail.com>
2021-04-12 05:47:07 +0400

    Added theme related constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,120 @@
 import 'package:flutter/material.dart';
 
+// Login/Signup screens properties
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0XFFF1E6FF);
+// Main app properties
+const colorBlack = Color(0xFF000000);
+const colorGrey = Color(0xFFBDBDBD);
+const colorWhite = Colors.white;
+const colorDarkBlue = Color(0xFF1A237E);
+// Styles we are going to be using throughout the app
+const TextTheme textThemeDefault = TextTheme(
+  headline1: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 26,
+  ),
+  headline2: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 22,
+  ),
+  headline3: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+  ),
+  headline4: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+  ),
+  headline5: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+  ),
+  headline6: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+  ),
+  bodyText1: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    height: 1.5,
+  ),
+  bodyText2: TextStyle(
+    color: colorGrey,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+    height: 1.5,
+  ),
+  subtitle1: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  ),
+  subtitle2: TextStyle(
+    color: colorGrey,
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  ),
+);
+// Theme properties for small devices
+const TextTheme textThemeSmall = TextTheme(
+  headline1: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 22,
+  ),
+  headline2: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+  ),
+  headline3: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 18,
+  ),
+  headline4: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+  ),
+  headline5: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+  ),
+  headline6: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w700,
+    fontSize: 10,
+  ),
+  bodyText1: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    height: 1.5,
+  ),
+  bodyText2: TextStyle(
+    color: colorGrey,
+    fontWeight: FontWeight.w500,
+    fontSize: 12,
+    height: 1.5,
+  ),
+  subtitle1: TextStyle(
+    color: colorBlack,
+    fontWeight: FontWeight.w400,
+    fontSize: 10,
+  ),
+  subtitle2: TextStyle(
+    color: colorGrey,
+    fontWeight: FontWeight.w400,
+    fontSize: 10,
+  ),
+);
